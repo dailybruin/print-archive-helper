d13e4916f01ac99eb4d0e78f180e0745e4a90690
brmi <desireelenart@gmail.com>
2016-10-18 20:50:27 -0700

    heroku changed database error

--- data_collection/data_app/settings.py
+++ data_collection/data_app/settings.py
@@ -72,7 +72,8 @@ WSGI_APPLICATION = 'data_app.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
+
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
