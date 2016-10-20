6c3ab162db27cc1201934b73c3aa078b207759ef
Zhang Hongyi <zhang.hongyi@outlook.com>
2016-10-20 16:44:10 -0700

    more frontend stuff

--- printarchive_app/static/script.js
+++ printarchive_app/static/script.js
@@ -1,7 +1,1 @@
-var g = $("#pdf-frame")
-//
-//.find("a:first").click();
-g.on('load', function() {
-  var loaded_btn = $("#pdf-frame").contents().find(".BRicon.zoom_in")
-  console.log(loaded_btn)
-});
+var pdfFrame = $("#pdf-frame")
